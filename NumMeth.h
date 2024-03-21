3e5a393f290ed130f0f4b4bf5e7ae8eb2e0157fc
Daniela Yano <danielayano@danielas-mbp.dyndns.rice.edu>
2024-03-21 12:10:51 -0500

    initial commit

---
+++ NumMeth.h
@@ -0,0 +1,8 @@
+// General header file for C++ programs 
+// in "Numerical Methods for Physics" 
+
+#include <iostream>
+#include <fstream>
+// #include <assert>  
+#include <cmath>
+// #include "Matrix.h"
